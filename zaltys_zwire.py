f727792309518eda2c38335e42f8f3fa7b647b3c
Paul Onions <paul_onions@siliconinfusion.com>
2016-11-18 15:10:27 +0000

    Added ZwireUART class.

--- zaltys_zwire.py
+++ zaltys_zwire.py
@@ -1,29 +1,35 @@
-##
-##  Author        : Paul Onions
-##  Creation date : 16 December 2015
-##
-##  Copyright 2015 - 2016 Silicon Infusion Limited
-##
-##  Silicon Infusion Limited                 
-##  CP House
-##  Otterspool Way
-##  Watford WD25 8HP
-##  Hertfordshire, UK
-##  Tel: +44 (0)1923 650404
-##  Fax: +44 (0)1923 650374
-##  Web: www.siliconinfusion.com
-##
-##  Licence: MIT, see LICENCE file for details.
-##
+#
+# Author        : Paul Onions
+# Creation date : 16 December 2015
+#
+# Copyright 2015 - 2016 Silicon Infusion Limited
+#
+# Silicon Infusion Limited                 
+# CP House
+# Otterspool Way
+# Watford WD25 8HP
+# Hertfordshire, UK
+# Tel: +44 (0)1923 650404
+# Fax: +44 (0)1923 650374
+# Web: www.siliconinfusion.com
+#
+# Licence: MIT, see LICENCE file for details.
+#
 
-##
-##  Interfaces to C SPI functions in /usr/lib/libzaltys-zwire.so
-##
+#
+# Running Zwire protocol over SPI and UART links.
+#
+# SPI routines interface to C SPI functions in libzaltys-zwire.so
+#
 
-import os
 import ctypes
+import ctypes.util
+import serial
 
 
+#
+# Zwire base class
+#
 class Zwire(object):
     '''
         Base class for Zwire SPI/other serial interfaces
@@ -31,18 +37,27 @@ class Zwire(object):
     def __init__(self):
         pass
 
-        
+
+#
+# Zwire over SPI
+#        
 class ZwireSPI(Zwire):
     '''
         Interface to ZwireSPI library functions
     '''
-    def __init__(self, dspi=0):
+    def __init__(self, dspi=0, libzaltys_zwire_path=None):
         '''
             Initialises .so library
         '''
-        self.bus = 'SPI'
-        self.dspi=dspi
-        self.lib = ctypes.CDLL('/usr/lib/libzaltys-zwire.so')
+        self.bus  = 'SPI'
+        self.dspi = dspi
+
+        if libzaltys_zwire_path:
+            self.libzaltys_zwire_path = libzaltys_zwire_path
+        else:
+            self.libzaltys_zwire_path = ctypes.util.find_library("libzaltys-zwire.so")
+
+        self.lib = ctypes.CDLL(self.libzaltys_zwire_path)
        
     def open(self):
         '''
@@ -102,6 +117,97 @@ class ZwireSPI(Zwire):
         self.lib.zwspiClose(ctypes.c_int(self.fd))
 
 
+#
+# Zwire over UART
+#
+# Note: for legacy compatibility reasons, the manner in which commands
+# and data values are encoded and transported over the serial link are
+# somewhat unusual.
+#
+class ZwireUART(Zwire):
+    '''
+        Zwire protocol over a UART serial line
+    '''
+    def __init__(self, port, baud=115200):
+        self.CMD_GETVER   = "$00"    # Read software ID & revision
+        self.CMD_SETEEP   = "$01"    # Write EEPROM data
+        self.CMD_GETEEP   = "$02"    # Read EEPROM data
+        self.CMD_SETBAUD  = "$03"    # Set Serial / USB BAUD rate
+        self.CMD_CFGFPGA  = "$04"    # Configure FPGA
+        self.CMD_GETRFPGA = "$05"    # Read FPGA SPI locations repetitively
+        self.CMD_GETSFPGA = "$06"    # Read FPGA SPI locations sequentially
+        self.CMD_SETRFPGA = "$07"    # Write FPGA SPI locations repetitively
+        self.CMD_SETSFPGA = "$08"    # Write FPGA SPI locations sequentially
+
+        self.port = port
+        self.baud = baud
+
+    def open(self):
+        '''
+            Open serial connection.  Return an 18-character version string
+            obtained from the remote endpoint.
+        '''
+        self.uart = serial.Serial(self.port, self.baud, timeout=1, writeTimeout=1)
+        self.uart.flushInput()
+        cmd = self.appendCmdChkSum(self.CMD_GETVER)
+        self.uart.write(bytes(cmd, encoding='ascii'))
+
+        # read the 18-byte response
+        response = ""
+        for n in range(18):
+            byte = self.uart.read()
+            response = response + byte.decode('ascii')
+        return response
+
+    def appendCmdChkSum(self, cmd):
+        check_sum = 0
+        for character in cmd:
+            check_sum = check_sum + ord(character)              
+        check_sum = check_sum % 256
+        full_cmd = cmd + ":" + hex(check_sum)[2:].zfill(2)
+        return full_cmd
+        
+    def read(self, address):
+        '''
+            Read integer value from 32-bit register.
+        '''
+        self.uart.flushInput()
+        count = 1
+        cmd = self.appendCmdChkSum(self.CMD_GETRFPGA + hex(address)[2:].zfill(8) + hex(count)[2:].zfill(4))
+        self.uart.write(bytes(cmd, encoding='ascii'))
+
+        # read the 4-byte response
+        response = [0]*4
+        for n in range(4):
+            response[n] = self.uart.read()[0]
+        
+        return response[0]*2**24 + response[1]*2**16 + response[2]*2**8 + response[3]
+
+    def write(self, address, data):
+        '''
+            Write integer data value to 32-bit register.
+        '''
+        self.uart.flushInput()
+        count = 1
+        cmd = self.appendCmdChkSum(self.CMD_SETRFPGA + hex(address)[2:].zfill(8) + hex(count)[2:].zfill(4))
+        byts = [0]*4
+        byts[0] = (data // 2**24) % 256
+        byts[1] = (data // 2**16) % 256
+        byts[2] = (data // 2**8) % 256
+        byts[3] = data % 256
+        self.uart.write(bytes(cmd, encoding='ascii'))
+        self.uart.write(bytearray(byts))
+
+    def close(self):
+        '''
+            Close serial connection.
+        '''
+        self.uart.close()
+
+
+#
+# A dummy Zwire connection for test purposes
+#
 class ZwireDummy(Zwire):
     '''
         A dummy Zwire interface for development purposes
